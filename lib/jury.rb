cee13f88b5e28c96d449dec7a4275b738272fb6b
Diane Van Etten <diane.vanetten@gmail.com>
2016-09-01 22:06:26 -0500

    Finish jury test

--- lib/jury.rb
+++ lib/jury.rb
@@ -3,6 +3,7 @@ class Jury
 
   def initialize
     @members = []
+    @finalists = {}
   end
 
   def add_member(member)
@@ -14,6 +15,24 @@ class Jury
       finalists[0] => 0,
       finalists[1] => 0
     }
+    members.each do |member|
+      vote = finalists.sample
+      puts vote
+      votes[vote] += 1
+    end
+    return votes
   end
-  
+
+  def report_votes(votes)
+    votes.each do |finalist, votes|
+      puts "#{finalist}: " + "#{votes}"
+    end
+  end
+
+  def announce_winner(votes)
+    finalists = votes.keys
+    winner = votes.max_by{|finalist, votes| votes}.first
+    puts "The winner is... #{winner.to_s.capitalize}"
+    winner
+  end
 end
