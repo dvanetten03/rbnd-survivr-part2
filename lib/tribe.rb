3ff722ae005ef1fa148e21ad579d29d539ff1919
Diane Van Etten <diane.vanetten@gmail.com>
2016-09-01 22:32:03 -0500

    tribe_test.rb test passes

---
+++ lib/tribe.rb
@@ -0,0 +1,20 @@
+class Tribe
+  attr_reader :name, :members
+
+  def initialize(options={})
+    @name = options[:name]
+    @members = options[:members]
+    puts "Tribe #{name} created."
+  end
+
+  def to_s
+    @name
+  end
+
+  def tribal_council(options={})
+    not_immune = @members.select {|member| member != options[:immune]}
+    eliminated = not_immune.sample
+    @members.delete(eliminated)
+    return eliminated
+  end
+end
